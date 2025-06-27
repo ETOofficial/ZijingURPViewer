f6cebb4d4b68535a612878c819860a0986ef47bf
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2025-06-27 17:13:44 +0800

    feat(exam): 添加考试信息查询功能
    - 新增 ExamFragment 和 ExamViewModel 用于考试信息查询
    - 更新导航菜单，移除不必要的选项，添加考试菜单项
    - 新增考试信息查询相关的字符串资源
    - 添加 StandardVPNCookies 类用于处理 VPN cookies

--- app/src/main/java/dynastxu/zijingurpviewer/MainActivity.java
+++ app/src/main/java/dynastxu/zijingurpviewer/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity{
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_login)
+                R.id.nav_home, R.id.nav_login, R.id.nav_exam)
                 .setOpenableLayout(drawer)
                 .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
