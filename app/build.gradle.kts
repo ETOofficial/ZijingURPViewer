25c0c9d4a1f52fb3e027bc29d40a7cf67e16ee50
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2025-06-30 18:01:15 +0800

    build: 更新应用版本信息
    - 将 versionCode 从 4 修改为 5
    - 将 versionName 从 "0.1.3_alpha" 修改为 "0.1.4_beta"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dynastxu.zijingurpviewer"
         minSdk = 31
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3_alpha"
+        versionCode = 5
+        versionName = "0.1.4_beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
